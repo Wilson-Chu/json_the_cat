c5cc8f6229403db0022810297d1e2bbf9e01e825
Wilson Chu <wilson1.chu@gmail.com>
2023-09-28 19:19:47 -0400

    implemented function and error handling for CAAS exercise

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,18 @@
 const request = require("request");
+
+const input = process.argv;
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${input[2]}`, (error, response, body) => {
+  const data = JSON.parse(body);
+
+  try {
+    console.log(`Description: ${data[0].description}`);
+  } catch (error) {
+    console.error('Error:', error); // Print the error if one occurred
+
+    if (data.length === 0) {
+      console.log(`Breed not found: ${input[2]}`);
+    }
+  }
+});
+
